1d59b98d2a0b8da12d27baa2c3e82451ebcced6a
LunaStev <youngjae681@gmail.com>
2024-12-09 15:52:29 +0900

    bug fix

--- src/transaction.rs
+++ src/transaction.rs
@@ -1,4 +1,5 @@
-use secp256k1::{Secp256k1, SecretKey};
+use secp256k1::{Secp256k1, SecretKey, Message};
+use sha2::{Sha256, Digest};
 
 pub struct TransactionInput {
     pub txid: String,
@@ -22,8 +23,16 @@ impl Transaction {
 
     pub fn sign(&self, private_key: &SecretKey) -> Vec<u8> {
         let secp = Secp256k1::new();
+
         let message = b"transaction_data";
-        let sig = secp.sign_ecdsa_low_r(&message.into(), private_key);
+        let mut hasher = Sha256::new();
+        hasher.update(message);
+        let hash = hasher.finalize();
+
+        let msg = Message::from_slice(&hash).expect("Failed to create message");
+
+        let sig = secp.sign_ecdsa_low_r(&msg, private_key);
+
         sig.serialize_compact().to_vec()
     }
 }
